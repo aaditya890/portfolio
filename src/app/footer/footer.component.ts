1de489ddcfee1c0d137e6ce45c27c79dafa0cfd3
aaditya890 <mishraaaditya297@gmail.com>
2026-02-19 18:45:04 +0530

    .

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  emailText: string = 'aadityamishra.dev@gmail.com';
     socialIcons: string[] = ['🌐', 'Be', 'in'];
 }
